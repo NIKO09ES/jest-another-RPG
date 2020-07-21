7a302a55d52abfbac6ddfdadde17aaa00be279b1
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-07-21 14:59:05 -0500

    add mock/potion

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,6 +1,6 @@
 const Potion = require('../lib/Potion.js');
 
-test('creates a random potion object', () => {
+test('creates a random potion object', () =>{
     const potion = new Potion();
 
     expect(potion.name).toEqual(expect.any(String));
